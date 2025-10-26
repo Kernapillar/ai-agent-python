6e33f81c77a1486c16f418d2d9da528fca536aec
Alexander Kern <alexander.t.kern@gmail.com>
2025-10-26 13:46:33 -0500

    verbose tag

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 if len(sys.argv) > 1 : 
-    input = sys.argv[1] 
+    user_prompt = sys.argv[1] 
+    # print(sys.argv)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
+    ]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if '--verbose' in sys.argv: 
+        print(f"User prompt: {response.text}")    
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else: 
+        print(response.text)
+
 else: 
     print("invalid empty prompt. Goodbye" )
     sys.exit(1)
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=input)]), 
-]
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
