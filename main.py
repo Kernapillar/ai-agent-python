ea080c56a26fa7252b937ce71dc58eedaf87e2db
Alexander Kern <alexander.t.kern@gmail.com>
2025-10-30 23:22:50 -0500

    manual testing get_files_info

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+print(get_files_info("calculator", "pkg"))
+# load_dotenv()
+# api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
-if len(sys.argv) > 1 : 
-    user_prompt = sys.argv[1] 
-    # print(sys.argv)
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
-    ]
+# client = genai.Client(api_key=api_key)
+# if len(sys.argv) > 1 : 
+#     user_prompt = sys.argv[1] 
+#     messages = [
+#         types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
+#     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-    if '--verbose' in sys.argv: 
-        print(f"User prompt: {response.text}")    
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else: 
-        print(response.text)
+#     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+#     if '--verbose' in sys.argv: 
+#         print(f"User prompt: {response.text}")    
+#         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+#         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+#     else: 
+#         print(response.text)
 
-else: 
-    print("invalid empty prompt. Goodbye" )
-    sys.exit(1)
+# else: 
+#     print("invalid empty prompt. Goodbye" )
+#     sys.exit(1)
 
 
