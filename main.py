0cc5eb00084069cc7c4e2b1a30ec9b96d2626b45
Alexander Kern <alexander.t.kern@gmail.com>
2025-11-04 10:40:44 -0600

    function calling get_file_info

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
 
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
+system_prompt = system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,13 +33,19 @@ if len(sys.argv) > 1 :
         types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
+    response = client.models.generate_content(model="gemini-2.0-flash-001", 
+                                                contents=messages, 
+                                                config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]))
     if '--verbose' in sys.argv: 
         print(f"User prompt: {response.text}")    
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else: 
-        print(response.text)
+        if len(response.function_calls) > 0: 
+            for function_call_part in response.function_calls: 
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else: 
+            print(response.text)
 
 else: 
     print("invalid empty prompt. Goodbye" )
